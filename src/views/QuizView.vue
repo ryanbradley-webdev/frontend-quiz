c1bf61db6992ad8541e4bc65c4cc345d4c7ec0e9
Ryan Bradley <ryanbradley315@gmail.com>
2023-11-17 18:15:40 -0600

    Add styling and functionality to interact with quiz, see correct/incorrect answers, and handle the end of the quiz.

--- src/views/QuizView.vue
+++ src/views/QuizView.vue
@@ -16,13 +16,33 @@ const {
 
 const questionNumber = ref(0)
 const currentQuestion = computed(() => quiz.questions[questionNumber.value])
+const selectedOption = ref('')
+const questionIsAnswered = ref(false)
+const questionIsCorrect = ref<boolean | null>(null)
+
+const submitAnswer = () => {
+  if (!selectedOption.value) {
+    return
+  }
+
+  questionIsAnswered.value = true
+  questionIsCorrect.value = selectedOption.value === currentQuestion.value.answer
+
+  console.log('hello')
+}
 
 const nextQuestion = () => {
+  questionIsAnswered.value = false
+
   if (questionNumber.value < 9) {
     questionNumber.value++
   } else {
     questionNumber.value = 0
   }
+}
+
+const seeResults = () => {
+
 }
 </script>
 
@@ -30,15 +50,23 @@ const nextQuestion = () => {
 
   <main>
 
-    <section>
+    <section
+      class="question-container"
+    >
 
-      <em>
-        Question {{ questionNumber + 1 }} of {{ quiz.questions.length }}
-      </em>
-
-      <h3>
-        {{ currentQuestion.question }}
-      </h3>
+      <div
+        class="question"
+      >
+  
+        <em>
+          Question {{ questionNumber + 1 }} of {{ quiz.questions.length }}
+        </em>
+  
+        <h3>
+          {{ currentQuestion.question }}
+        </h3>
+        
+      </div>
 
       <ProgressBar
         :currentQuestion="questionNumber + 1"
@@ -47,16 +75,41 @@ const nextQuestion = () => {
 
     </section>
 
-    <section>
+    <section
+      class="options"
+    >
 
-      <div v-for="option in currentQuestion.options">
-        {{ option }}
-      </div>
+      <label
+        class="option-container"
+        v-for="option in currentQuestion.options"
+        :data-incorrect-answer="questionIsAnswered && selectedOption === option && currentQuestion.answer !== option"
+        :data-correct-answer="questionIsAnswered && option === currentQuestion.answer"
+        :key="option"
+      >
+        <span>{{ option }}</span>
+        <input
+          type="radio"
+          name="options"
+          :id="option"
+          :value="option"
+          @change="() => selectedOption = option"
+        >
+      </label>
 
       <Button
-        @btn-click="nextQuestion"
+        @btn-click="() => {
+          if (questionIsAnswered) {
+            if (questionNumber === quiz.questions.length - 1) {
+              seeResults()
+            } else {
+              nextQuestion()
+            }
+          } else {
+            submitAnswer()
+          }
+        }"
       >
-        Next Question
+        {{ questionIsAnswered ? questionNumber === quiz.questions.length - 1 ? 'See Results' :  'Next Question' : 'Submit Answer' }}
       </Button>
 
     </section>
@@ -66,5 +119,98 @@ const nextQuestion = () => {
 </template>
 
 <style scoped>
+.question-container {
+  display: grid;
+  row-gap: 24px;
+}
 
+.question {
+  display: grid;
+  row-gap: 12px;
+}
+
+.options {
+  display: grid;
+  row-gap: 12px;
+}
+
+.option-container {
+  position: relative;
+  display: flex;
+  align-items: center;
+  gap: 16px;
+  padding: 12px;
+  width: 100%;
+  height: 64px;
+  color: inherit;
+  background-color: var(--clr-white-pri);
+  border-radius: 12px;
+  text-decoration: none;
+  font-size: var(--font-size-head-sm);
+  font-weight: 500;
+  box-shadow: 0 16px 40px #8fa0c211;
+}
+
+.option-container input {
+  display: none;
+}
+
+.option-container::before {
+  display: grid;
+  place-items: center;
+  flex-shrink: 0;
+  height: 40px;
+  width: 40px;
+  color: var(--clr-grey-navy);
+  background-color: var(--clr-light-grey);
+  border-radius: 6px;
+}
+
+.option-container:first-child::before {
+  content: 'A';
+}
+
+.option-container:nth-child(2)::before {
+  content: 'B';
+}
+
+.option-container:nth-child(3)::before {
+  content: 'C';
+}
+
+.option-container:nth-child(4)::before {
+  content: 'D';
+}
+
+.option-container:has(input:checked)::after,
+.option-container[data-correct-answer="true"]::after,
+.option-container[data-incorrect-answer="true"]::after {
+  content: '';
+  position: absolute;
+  inset: 0;
+  border: 3px solid var(--clr-purple-pri);
+  border-radius: 12px;
+}
+
+.option-container:has(input:checked)::before {
+  color: var(--clr-white-pri);
+  background-color: var(--clr-purple-pri);
+}
+
+label.option-container[data-correct-answer="true"]::after {
+  border-color: var(--clr-green-pri);
+}
+
+label.option-container[data-correct-answer="true"]::before {
+  color: var(--clr-white-pri);
+  background-color: var(--clr-green-pri);
+}
+
+.option-container[data-incorrect-answer="true"]:has(input:checked)::after {
+  border-color: var(--clr-red-pri);
+}
+
+.option-container[data-incorrect-answer="true"]:has(input:checked)::before {
+  background-color: var(--clr-red-pri);
+}
 </style>
